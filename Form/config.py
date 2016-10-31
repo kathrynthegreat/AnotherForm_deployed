70c452bfd564e2acdce1070642fbcfbe995e2cb9
Katya Vasilaky <kvasilaky@gmail.com>
2016-10-31 15:22:21 -0400

    changed the name of db url from DB_URL to DATABASE_URL in .config

--- Form/config.py
+++ Form/config.py
@@ -4,4 +4,4 @@ import logging
 #http://www.blog.pythonlibrary.org/2012/08/02/python-101-an-intro-to-logging/
 logging.basicConfig(level=logging.INFO)
 
-DB_URL = os.environ["DB_URL"]
+DB_URL = os.environ["DATABASE_URL"]
